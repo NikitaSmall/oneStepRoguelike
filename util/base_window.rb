41d990d3b02292f678d76ef18457329d374f2bbf
nikitasmall <nikitasosnov@yahoo.com>
2015-03-29 00:02:46 +0200

    tile => point, because of conflict with libtcod class name. Doesn't matter, but w/o red lines in code

--- util/base_window.rb
+++ util/base_window.rb
@@ -1,19 +1,4 @@
-#require './util/tile'
-class Point
-  attr_accessor :char, :foreground, :background
-
-  def initialize(char=' ', foreground=TCOD::Color::LIGHTER_GRAY, background=TCOD::Color::BLACK)
-    @char = char
-    @foreground = foreground
-    @background = background
-  end
-
-  def set_char(char, foreground, background)
-    @char = char
-    @foreground = foreground
-    @background = background
-  end
-end
+require './util/tile'
 
 class BaseWindow
   attr_accessor :id, :x, :y, :h, :w, :tiles, :methods
--- util/tile.rb
+++ util/tile.rb
@@ -1,8 +1,10 @@
 class Point
   attr_accessor :char, :foreground, :background
 
-  def initialize
-    @char = ' '
+  def initialize(char=' ', foreground=TCOD::Color::LIGHTER_GRAY, background=TCOD::Color::BLACK)
+    @char = char
+    @foreground = foreground
+    @background = background
   end
 
   def set_char(char, foreground, background)
